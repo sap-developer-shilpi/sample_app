0ffc16bf6f8f431e45c8aacabe7e495b8bc4eb7c
Shilpi Sharma <sap.developer.shilpi@gmail.com>
2017-08-19 15:08:46 +0200

    Implement advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,11 @@ class SessionsController < ApplicationController
   end
 
   def create
+    puts "Email from text box " + params[:session][:email]
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
+	  params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user # Log the user in and redirect to the user's show page.
     else
       flash.now[:danger] = 'Invalid email/password combination' # Create an error message.
@@ -14,7 +16,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
