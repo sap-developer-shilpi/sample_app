53feeb025231ad8b65d43b9f1cfc4a869825dbc8
Shilpi Sharma <sap.developer.shilpi@gmail.com>
2017-08-22 12:23:51 +0200

    add account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,13 +13,11 @@ class UsersController < ApplicationController
   @user = User.new
   end
   def create
-
     @user = User.new(user_params)
     if @user.save
-	log_in @user
-	flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
-	  # Handle a successful save.
+	  @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
       render 'new'
     end
@@ -62,4 +60,5 @@ class UsersController < ApplicationController
 	def admin_user
       redirect_to(root_url) unless current_user.admin?
     end
+	
 end
