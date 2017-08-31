1df332276882598a286f7b06fc005a40dd7d0ebc
Shilpi Sharma <sap.developer.shilpi@gmail.com>
2017-08-31 21:20:55 +0200

    add user microports

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   end
   def show
     @user = User.find(params[:id])
+	@microposts = @user.microposts.paginate(page: params[:page])
   end
   def new
   @user = User.new
@@ -39,6 +40,7 @@ class UsersController < ApplicationController
     flash[:success] = "User deleted"
     redirect_to users_url
   end
+  
 
   private
 
@@ -46,13 +48,7 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
-	def logged_in_user
-      unless logged_in?
-	    store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
+	
 	def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
